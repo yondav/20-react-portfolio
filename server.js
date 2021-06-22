187e94b5312754b8a9a0bdf4a9d2fee44cd455f5
yondav <yonidavid.abr@gmail.com>
2021-06-22 18:55:26 -0400

    repairing path for serving build directory to heroku

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(cors());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('portfolio/build'));
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'portfolio', 'build', 'index.html'));
   });
 }
 
