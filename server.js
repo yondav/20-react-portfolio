26bff88114f5b2b94914567f42a92d85889e5de7
yondav <yonidavid.abr@gmail.com>
2021-06-22 18:49:41 -0400

    fixed Get request for serverside to deploy to heroku. fixed css bug on contact page

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // nodemailer transporter
@@ -56,10 +61,6 @@ app.post('/send', function (req, res) {
     }
   });
 });
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
 
 // Start the API server
 app.listen(PORT, () =>
