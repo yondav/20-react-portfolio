9ac50121767a666c31af59b9d0b5f2e162a21f13
yondav <yonidavid.abr@gmail.com>
2021-06-23 11:40:44 -0400

    meta tags dynamically rendered in App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MetaTags from 'react-meta-tags';
 
 import Home from './components/Pages/Home/Home';
 import Nav from './components/Nav/Nav';
@@ -11,17 +12,53 @@ import Contact from './components/Pages/Contact/Contact';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/dev' component={Dev} />
-        <Route exact path='/portraits' component={Portraits} />
-        <Route exact path='/contact' component={Contact} />
-        <Socials />
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <MetaTags>
+        <title>yondav</title>
+        <meta
+          name='description'
+          content='Yoni David - Full Stack Web Developer & Visual Designer'
+        />
+        <meta itemProp='name' content='yondav' />
+        <meta
+          itemProp='description'
+          content='Yoni David - Full Stack Web Developer & Visual Designer'
+        />
+        <meta itemProp='image' content='https://www.yupny.com/img/yoni2.jpg' />
+        <meta property='og:url' content='https://www.yondav.us' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='yondav' />
+        <meta
+          property='og:description'
+          content='Yoni David - Full Stack Web Developer & Visual Designer'
+        />
+        <meta
+          property='og:image'
+          content='https://www.yupny.com/img/yoni2.jpg'
+        ></meta>
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='yondav' />
+        <meta
+          name='twitter:description'
+          content='Yoni David - Full Stack Web Developer & Visual Designer'
+        />
+        <meta
+          name='twitter:image'
+          content='https://www.yupny.com/img/yoni2.jpg'
+        ></meta>
+      </MetaTags>
+      <Router>
+        <div>
+          <Nav />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/dev' component={Dev} />
+          <Route exact path='/portraits' component={Portraits} />
+          <Route exact path='/contact' component={Contact} />
+          <Socials />
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 };
 
